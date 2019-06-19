ba598d627d2d9b651e4ecf265fd13e131b4956a1
WebWolfy <46201442+MatthijsReyers@users.noreply.github.com>
2019-06-19 17:11:51 +0200

    Create 19-06-2019.py

---
+++ 19-06-2019.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+# THE DAILY PROBLEM
+# -------------------------------------------------------------------------------------
+# This problem was recently asked by Google. Given a list of numbers and a number k, 
+# return whether any two numbers from the list add up to k.
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+#
+# Bonus: Can you do this in one pass?
+
+
+L = [10, 15, 3, 7]
+K = 19
+
+def FindSimple(l,k):
+    for n in l:
+        L2 = l.copy()
+        L2.remove(n)
+        for a in L2:
+            if (n + a) == k: return True
+    return False
+
+def FindOnePass(l,k):
+    for n in l:
+        try: 
+            l.index(k-n)
+            return True
+        except ValueError:
+            pass
+    return False
+
+if __name__ == '__main__': 
+    print(FindOnePass(L,K))
